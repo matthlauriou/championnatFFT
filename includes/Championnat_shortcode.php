bdb9959f32f9801f5400e3ae89eb42bb65001a0a
Psychee <orelydbm@gamil.com>
2022-04-15 09:58:57 +0200

    recupération des données en base par le shortcode ok et reglage des bugs

--- includes/Championnat_shortcode.php
+++ includes/Championnat_shortcode.php
@@ -1,19 +1,16 @@
 <?php
-
-
-	require plugin_dir_path(__FILE__).'/bdd/Requetes.php';
-       
-
-
+    
     // Fonction qui s'applique quand le shortcode est activé 
     function championnatFFT_resultats($atts) { 
-        //le coté adlmin de wordpress est en erreur si je met $requetes et require si je ne les met pas l'appel a la base de données ne se fait pas 
+
+        require plugin_dir_path(__FILE__).'/bdd/Requetes.php';
+        require plugin_dir_path(__FILE__).'/constantes/constantes.php';
         global $wpdb;
         $requetes = new Requetes($wpdb);
-        
-        //Se que l'on veut que la fonction fasse ici pour le test afficher "hello world"
+      
         //1 récupérer les données de l'équipe
-        //recupérer l'id de l'equipe //numéro_equipe pcq id présent que dans notre bdd alors que numero_equipe est une donnée commune avec tenup
+
+        //recupérer l'id de l'equipe 
         $atts = shortcode_atts( array(
             'id'=>''//doit recupérer l'attribut id du shortcode
             ),$atts,'championnatFFT');
@@ -25,33 +22,46 @@
 
         //quand on met l'attribut id dans le shortcode cela declanche une erreur signalant un json invalide
         $idEquipe = $atts['id']; //doit changer en fonction de l'id recupérer dans le shortcode $idEquipe = $atts['id'];
+        print_r($idEquipe);
         //utiliser la fonction getEquipe($idEquipe)
         $equipe = $requetes->getEquipe($idEquipe);
         //valoriser les variables
-        //print_r($equipe);
+        print_r($equipe);
 
         $numero_championnat = $equipe->numero_championnat;
         $division_championnat = $equipe->division_championnat; 
         $phase_championnat = $equipe->phase_championnat;
         $poule_championnat = $equipe->poule_championnat;
-
-        //print_r($numero_championnat);
+        //$numero_equipe = $equipe->numero_equipe;
         
-        //2 récupérer les données de parametrage 
+        //2 récupérer les données de parametrage
+
         //utiliser la fonction getAllParametrage
         $listeParametres = $requetes->getAllParametrages();
         print_r($listeParametres);
         // on as donc les valeur de $URL_POST, $URL_FEUILLE_MATCH
         //recupère la clé URL_POST dont la $valeur est en base
+        foreach ($listeParametres as $parametre) {
+            $cle = $parametre->cle;
+            $valeur = $parametre->valeur;
+    
+            if (strcmp($cle, $URL_POST) == 0) {
+                $url_post = $valeur;
+            } elseif (strcmp($cle, $URL_FEUILLE_MATCH) == 0) {
+                $url_feuille_match = $valeur;
+            }
+        }
+        
+        print_r($url_post);
 
-        $url_post='https://tenup.fft.fr/championnat/request/ajax';
         //3 établir le curl 
+
         //simulation d'un remplissage de formulaire!!
 
         //l'url ou on veut recupérer les données    la requète en mode POST en fonction de la feuille de match choisi $url_feuille_match et du pattern $url_post
         $url = $url_post;
         //Les données que l'on veut envoyer en POST 
-        $data = array('fiche_championnat'=>$numero_championnat,'division'=>$division_championnat,'phase'=>$phase_championnat,'poule'=>$poule_championnat,'formSubmit'=>true);
+        $data = array('fiche_championnat'=>$numero_championnat,'division'=>$division_championnat,'phase'=>$phase_championnat,'poule'=>$poule_championnat,'formSubmit'=>true); //rajouter 'equipe'=>$numero_equipe
         //la requete 
         $option = array(
             'http'=> array( 
@@ -66,7 +76,7 @@
 
         //4 récupérer le fichier le JSON
         $context = stream_context_create($option);
-        //on transforme le jsonen phrase
+        //on transforme le json en phrase
         $result = file_get_contents($url, false, $context);
         //arriver a parser le json utilise json_decode pour extraire les informations de la phrase json creer et recuperer par le file_get_contents() le json_encode() serai pour passer des données php en phrase json
         //$decode_json = json_decode($result,true);//true retourne un tableau d'objets false un objets ,null objets ou tableau d'objets si const JSON_OBJECT_AS_ARRAY à été defini dans le parametre flags
@@ -91,7 +101,7 @@
         return $result;
         
         //arriver a n'avoir que le body dans la réponse json ?
-   
+
     }
     // ajouter shortcode
     add_shortcode('championnatFFT', 'championnatFFT_resultats');
